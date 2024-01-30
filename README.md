43da60cb621117125390a665b8144adccf23f971
Kevin Ford <kevinford@loc.gov>
2024-01-30 17:37:35 -0500

    Finessing; instructions.

---
+++ README.md
@@ -0,0 +1,8 @@
+
+```bash
+
+git clone 
+./bin/install.sh
+./bin/run_tests.sh https://id.loc.gov
+
+```
--- bin/install.sh
+++ bin/install.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR/../lib/
 curl -OL https://files.basex.org/releases/10.7/BaseX107.jar
-cd $DIR
+cd $DIR/../
